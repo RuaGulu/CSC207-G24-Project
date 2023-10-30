c5794a09ce40411b61ce9343d306c6544e3b2333
RuaGulu <56189742+RuaGulu@users.noreply.github.com>
2023-10-30 10:49:02 -0400

    Created usecases for Trip

---
+++ src/use_case/trip/TripInteractor.java
@@ -0,0 +1,16 @@
+package use_case.trip;
+
+import entity.Trip;
+
+import java.time.LocalDateTime;
+
+// TODO
+public class TripInteractor implements TripInputBoundary{
+
+
+
+    @Override
+    public void execute(TripInputData TripInputData) {
+
+    }
+}
---
+++ src/use_case/trip/TripOutputData.java
@@ -0,0 +1,6 @@
+package use_case.trip;
+
+// TODO
+public class TripOutputData {
+
+}
