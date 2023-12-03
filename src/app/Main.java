3e4b5259332edcb0e90494c9daa3900132646a73
FoliaZhang <144532806+FoliaZhang@users.noreply.github.com>
2023-12-02 20:34:38 -0500

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,17 +3,16 @@ package app;
 import api.APIDataAccessObject;
 import api.WeatherDB;
 import data_access.FilerUserDataAccessObject;
+import data_access.GroupDataAccessObject;
 import entity.CommonGroupFactory;
 import entity.CommonUserFactory;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.air_quality.AirQualityViewModel;
-import interface_adapter.create_group.CreateGroupViewModel;
+import interface_adapter.group.GroupViewModel;
 import interface_adapter.logged_in.LoggedInViewModel;
 import interface_adapter.login.LoginViewModel;
-import interface_adapter.search_usergroup.SearchUserGroupViewModel;
 import interface_adapter.signup.SignupViewModel;
 import interface_adapter.weather.WeatherViewModel;
-import view.GroupView;
 import view.LoggedinView;
 import view.LoginView;
 import view.SignupView;
@@ -25,6 +24,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+
         JFrame application = new JFrame("system");
         application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
@@ -39,41 +39,34 @@ public class Main {
         LoginViewModel loginViewModel = new LoginViewModel();
         LoggedInViewModel loggedinViewModel = new LoggedInViewModel();
         SignupViewModel signupViewModel = new SignupViewModel();
-        CreateGroupViewModel createGroupViewModel = new CreateGroupViewModel();
-        SearchUserGroupViewModel searchUserGroupViewModel = new SearchUserGroupViewModel();
         WeatherViewModel weatherViewModel = new WeatherViewModel();
         AirQualityViewModel airQualityViewModel = new AirQualityViewModel();
+        GroupViewModel groupViewModel = new GroupViewModel();
 
         FilerUserDataAccessObject userDataAccessObject;
         FilerUserDataAccessObject loginUserDataAccessObject;
         FilerUserDataAccessObject loggedInUserDataAccessObject;
-        FilerUserDataAccessObject groupFindGroupDataAccessObject;
-        FilerUserDataAccessObject searchGroupUserDataAccessObject;
-        FilerGroupDataAccessObject groupDataAccessObject;
+        GroupDataAccessObject groupDataAccessObject;
+        WeatherDB weatherDataAccessObject;
 
         try {
-            userDataAccessObject = new FilerUserDataAccessObject("./users.csv", new CommonUserFactory());
-            loginUserDataAccessObject = new FilerUserDataAccessObject("./users.csv", new CommonUserFactory());
-            loggedInUserDataAccessObject = new FilerUserDataAccessObject("./users.csv", new CommonUserFactory());
-            groupFindGroupDataAccessObject = new FilerUserDataAccessObject("./users.csv", new CommonUserFactory());
-            searchGroupUserDataAccessObject = new FilerUserDataAccessObject("./users.csv", new CommonUserFactory());
-            groupDataAccessObject = new FilerGroupDataAccessObject("./groups.csv", new CommonGroupFactory());
-
+            userDataAccessObject = new FilerUserDataAccessObject("./users.csv", new CommonUserFactory(), new CommonGroupFactory());
+            loginUserDataAccessObject = new FilerUserDataAccessObject("./users.csv", new CommonUserFactory(),new CommonGroupFactory());
+            loggedInUserDataAccessObject = new FilerUserDataAccessObject("./users.csv", new CommonUserFactory(),new CommonGroupFactory());
+            groupDataAccessObject = new GroupDataAccessObject("./groups.csv",new CommonGroupFactory(),new CommonUserFactory());
+            weatherDataAccessObject = new APIDataAccessObject();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        SignupView signupView = SignupUseCaseFactory.create(viewManagerModel, loginViewModel, signupViewModel, userDataAccessObject, loginUserDataAccessObject, loggedinViewModel,weatherViewModel );
+        SignupView signupView = SignupUseCaseFactory.create(viewManagerModel, loginViewModel, signupViewModel, userDataAccessObject, loginUserDataAccessObject, loggedinViewModel,weatherViewModel,groupDataAccessObject );
         views.add(signupView,signupView.viewName);
 
-        LoginView loginView = LoginUseCaseFactory.create(viewManagerModel,loginViewModel,loggedinViewModel,loginUserDataAccessObject,weatherViewModel);
+        LoginView loginView = LoginUseCaseFactory.create(viewManagerModel,loginViewModel,loggedinViewModel,loginUserDataAccessObject,weatherViewModel,groupDataAccessObject);
         views.add(loginView,loginView.viewName);
 
-        LoggedinView loggedinView = LoggedInUseCaseFactory.create(viewManagerModel,loggedinViewModel,weatherViewModel, airQualityViewModel, loggedInUserDataAccessObject,weatherDataAccessObject);
+        LoggedinView loggedinView = LoggedInUseCaseFactory.create(viewManagerModel,loggedinViewModel,weatherViewModel,airQualityViewModel,loggedInUserDataAccessObject,weatherDataAccessObject,groupViewModel,groupDataAccessObject);
         views.add(loggedinView,loggedinView.viewName);
-
-        GroupView groupView = GroupUseCaseFactory.create(viewManagerModel,createGroupViewModel,searchUserGroupViewModel, groupFindGroupDataAccessObject, groupDataAccessObject, searchGroupUserDataAccessObject);
-        views.add(groupView,groupView.viewName);
 
         viewManagerModel.setActiveView(signupView.viewName);
         viewManagerModel.firePropertyChanged();
