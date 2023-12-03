b9dd91cb7105d05cb99169f7ad4a012a5c696797
RuaGulu <56189742+RuaGulu@users.noreply.github.com>
2023-12-03 04:27:15 -0500

    Updated LoginUserDataAccessInterface for making tests working.

--- src/use_case/login/LoginUserDataAccessInterface.java
+++ src/use_case/login/LoginUserDataAccessInterface.java
@@ -1,8 +1,11 @@
 package use_case.login;
 
+import entity.CommonUser;
 import entity.User;
 
 public interface LoginUserDataAccessInterface {
     boolean existsByName(String identifier);
     User get(String username);
+
+    void save(User User);
 }
